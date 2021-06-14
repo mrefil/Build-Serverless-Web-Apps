493e01a5919aad2694b538d030c68a499a666c8d
mrefil <muefiloglu@protonmail.com>
2021-06-14 19:24:24 +0300

    60. Navigating with the "Link" Component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import styles from "../styles/Home.module.css";
+import Link from 'next/link';
 
 function HomePage() {
   return (
     <div className={styles.container}>
-      <h1>Hello World!</h1>
+      <h1>Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
   );
 }
