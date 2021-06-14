298640118e950ff44cde5ef6917fe3b1a0bf17f8
mrefil <muefiloglu@protonmail.com>
2021-06-14 19:32:11 +0300

    61. Navigating To Dynamic Routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,6 +1,20 @@
+import Link from 'next/link';
+
 function ClientsPage() {
+    const clients = [
+        {id: "mehmet", name: "Mehmet"},
+        {id: "ahmet", name: "Ahmet"}
+    ];
+
     return <div>
         <h1>Clients Page</h1>
+        <ul>
+            {clients.map((client) => (
+                <li key={client.id}>
+                    <Link href={`/clients/${client.id}`}>{client.name}</Link>
+                </li>
+            ))}
+        </ul>
     </div>
 }
 
