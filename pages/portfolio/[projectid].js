1650132653af02f97541e539ab730082092414fe
mrefil <muefiloglu@protonmail.com>
2021-06-14 18:49:38 +0300

    Extracting Dynamic Path Segment Data (Dynamic Routes)

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,4 +1,12 @@
+import { useRouter } from 'next/router';
+
 function PortfolioProjectPage() {
+    const router = useRouter();
+    console.log(router.pathname);
+    console.log(router.query);
+
+    // router.query.projectid
+
     return <di>
         <h1>Portfolio Project Page</h1>
     </di>
