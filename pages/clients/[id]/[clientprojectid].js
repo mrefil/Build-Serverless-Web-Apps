ca3ee51d6bb92f241a50d29b954a30c1404781ca
mrefil <muefiloglu@protonmail.com>
2021-06-15 20:46:57 +0300

    68. Planning The Project

--- pages/clients/[id]/[clientprojectid].js
+++
@@ -1,7 +0,0 @@
-function SelectedClientProjectPage() {
-    return <di>
-        <h1>Selected Client Projectt Page</h1>
-    </di>
-}
-
-export default SelectedClientProjectPage;
--- pages/portfolio/list.js
+++
@@ -1,7 +0,0 @@
-function ListPage() {
-    return <div>
-        <h1>List Page</h1>
-    </div>
-}
-
-export default ListPage;
